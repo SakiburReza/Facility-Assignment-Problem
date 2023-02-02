c9b6d60a4cce436c6594b52098111b60b3133a9b
Zahin112 <109434296+Zahin112@users.noreply.github.com>
2023-02-02 20:38:55 +0600

    Update and rename part_cost.java to part_assignment.java

--- part_assignment.java
+++ part_assignment.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class part_cost {
+public class part_assignment {
     //public Optimal opt;
     public static void main(String[] args) throws IOException{
 
